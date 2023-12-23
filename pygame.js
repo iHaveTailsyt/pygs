b92e438c57e1cdd6cba43d82ef462867e6bddf2a
iHaveTailsyt <127070806+iHaveTailsyt@users.noreply.github.com>
2023-12-23 15:07:26 -0500

    Update and rename pygs.py to pygame.js

---
+++ pygame.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const esprima = require('esprima');
+
+class PyGameInterpreter {
+  constructor() {
+    this.scope = {};
+  }
+
+  visit(node) {
+    if (node.type === 'Program') {
+      node.body.forEach(statement => this.visit(statement));
+    } else if (node.type === 'VariableDeclaration') {
+      node.declarations.forEach(declaration => {
+        this.scope[declaration.id.name] = this.visit(declaration.init);
+      });
+    } else if (node.type === 'Literal') {
+      return node.value;
+    } else if (node.type === 'Identifier') {
+      return this.scope[node.name];
+    } else if (node.type === 'BinaryExpression') {
+      const left = this.visit(node.left);
+      const right = this.visit(node.right);
+
+      switch (node.operator) {
+        case '+':
+          return left + right;
+        case '-':
+          return left - right;
+        case '*':
+          return left * right;
+        case '/':
+          return left / right;
+        default:
+          throw new Error(`Unsupported operator: ${node.operator}`);
+      }
+    } else {
+      throw new Error(`Unsupported node type: ${node.type}`);
+    }
+  }
+
+  runPyGameScript(code) {
+    const ast = esprima.parseScript(code);
+    this.visit(ast);
+  }
+}
+
+function runPyGameScriptFromFile(filename) {
+  const code = fs.readFileSync(filename, 'utf-8');
+  const interpreter = new PyGameInterpreter();
+  interpreter.runPyGameScript(code);
+}
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node pygameinterpreter.js your_script.pygs');
+  process.exit(1);
+}
+
+runPyGameScriptFromFile(process.argv[2]);
