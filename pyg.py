e514e45e7c282ca3d3ce79aa220db594ee944e68
iHaveTailsyt <127070806+iHaveTailsyt@users.noreply.github.com>
2023-12-23 15:19:05 -0500

    Update pyg.py

--- pyg.py
+++ pyg.py
@@ -4,6 +4,41 @@ import sys
 
 PYG_PACKAGES_DIR = 'pyg_packages'
 
+class PyGameInterpreter:
+    def __init__(self):
+        self.scope = {}
+
+    def visit(self, node):
+        if node['type'] == 'Program':
+            for statement in node['body']:
+                self.visit(statement)
+        elif node['type'] == 'VariableDeclaration':
+            for declaration in node['declarations']:
+                self.scope[declaration['id']['name']] = self.visit(declaration['init'])
+        elif node['type'] == 'Literal':
+            return node['value']
+        elif node['type'] == 'Identifier':
+            return self.scope.get(node['name'], None)
+        elif node['type'] == 'BinaryExpression':
+            left = self.visit(node['left'])
+            right = self.visit(node['right'])
+            if node['operator'] == '+':
+                return left + right
+            elif node['operator'] == '-':
+                return left - right
+            elif node['operator'] == '*':
+                return left * right
+            elif node['operator'] == '/':
+                return left / right
+        else:
+            print(f"Unsupported node type: {node['type']}")
+            sys.exit(1)
+
+    def run_pygame_script(self, code):
+        ast = json.loads(code)
+        for statement in ast['body']:
+            self.visit(statement)
+
 def read_package_json(package_name):
     package_dir = os.path.join(PYG_PACKAGES_DIR, package_name)
     package_json_path = os.path.join(package_dir, 'pyg.json')
@@ -26,12 +61,6 @@ def install_package(package_name, global_install=False):
 
     print(f'Package {package_name} successfully installed.')
 
-def run_pygame_script(filename):
-    with open(filename, 'r') as file:
-        code = file.read()
-    # Implement your PyGameScript interpreter logic here
-    print(f'Running PyGameScript: {filename}')
-
 def main():
     if len(sys.argv) < 2:
         print('Usage: python pyg.py <command> [arguments]')
@@ -50,7 +79,10 @@ def main():
             print('Usage: python pyg.py run script.pygs')
             sys.exit(1)
         script_filename = sys.argv[2]
-        run_pygame_script(script_filename)
+        with open(script_filename, 'r') as file:
+            code = file.read()
+        interpreter = PyGameInterpreter()
+        interpreter.run_pygame_script(code)
     else:
         print(f'Error: Unknown command "{command}"')
         sys.exit(1)
